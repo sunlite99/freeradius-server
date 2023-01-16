f3fdbd5976661677dfed2364e37ac3b5d9c95e70
sunlite99 <35430055+sunlite99@users.noreply.github.com>
2023-01-16 09:27:25 -0700

    Revert "removed duplicate assignment"
    This reverts commit 6610ae0087349f3a7f75f1a063467898c24f9239.

--- src/bin/radsniff.c
+++ src/bin/radsniff.c
@@ -2341,6 +2341,9 @@ int main(int argc, char *argv[])
 	conf->print_packet = true;
 	conf->limit = 0;
 	conf->promiscuous = true;
+#ifdef HAVE_COLLECTDC_H
+	conf->stats.prefix = RS_DEFAULT_PREFIX;
+#endif
 	conf->radius_secret = talloc_strdup(conf, RS_DEFAULT_SECRET);
 	conf->logger = NULL;
 
